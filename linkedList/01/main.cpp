7a13e4b7770d3ba8be97b95fcacefa1525767d18
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-11-30 02:51:10 -0300

    Adiciona o arquivo linkedList/01/Makefile

---
+++ linkedList/01/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <memory>
+
+struct Node {
+    int data;
+    std::unique_ptr<Node> next;
+
+    Node(int data) : data(data), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    std::unique_ptr<Node> head;
+
+public:
+    void append(int data) {
+        if (!head) {
+            head = std::make_unique<Node>(data);
+        } else {
+            Node* current = head.get();
+            while (current->next) {
+                current = current->next.get();
+            }
+            current->next = std::make_unique<Node>(data);
+        }
+    }
+
+    void print() const {
+        Node* current = head.get();
+        while (current) {
+            std::cout << current->data << " ";
+            current = current->next.get();
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    list.print(); // Saída: 1 2 3
+    return 0;
+}
+
