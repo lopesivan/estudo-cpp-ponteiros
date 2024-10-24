46781f133bf7f0b16efe51ef47bca3526f4c1e87
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-10-23 22:07:24 -0300

    Atualiza o arquivo new.sh

--- new.sh
+++ new.sh
@@ -4,5 +4,5 @@ ls -rtl *.cpp |
 	cut -d" " -f9 |
 	sort -n |
 	tail -1 |
-	sed 's/ex-\([0-9]\+\).cpp/\1/' |
+	sed -e 's/ex-\([0-9]\+\).cpp/\1/' -e 's/^0//' |
 	while read n; do printf "cp ex-%02d.cpp ex-%02d.cpp\n" "$n" "$((n + 1))"; done
